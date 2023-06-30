d1d54eb33bf0273a1d0ca324817e5f9c04e34235
Dark Emrys <mathersmarshall948@gmail.com>
2023-06-30 12:08:13 +0100

    implement createBrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
+
+import MainPage from './pages/MainPage.jsx'
+import Team from './pages/Team.jsx'
+import Membership from './pages/Membership.jsx'
+import About from './pages/About.jsx'
+import App from './layout/App.jsx'
 import './index.css'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={ <App /> }>
+      <Route index element={ <MainPage /> } />
+      <Route path='team' element={ <Team /> } />
+      <Route path='membership' element={ <Membership /> } />
+      <Route path='about' element={ <About /> } />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter >
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
