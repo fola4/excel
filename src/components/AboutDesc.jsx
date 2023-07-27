9c27980dafa01d0b8d8ce71accf98422b8c19117
UnsuspectedNoob <mathersmarshall948@gmail.com>
2023-07-27 11:14:01 +0100

    update aboutDesc.jsx

--- src/components/AboutDesc.jsx
+++ src/components/AboutDesc.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function AboutDesc({ letter, description }) {
   return (
-    <div className="mx-auto grid max-w-xl grid-cols-1 items-center gap-3 sm:grid-cols-[100px_1fr] ">
+    <div className="mx-auto grid max-w-xl grid-cols-1 items-center gap-8 sm:grid-cols-[100px_1fr] ">
       <span className="text-center text-9xl font-bold opacity-60 md:w-28">
         {letter}
       </span>
-      <p className="text-center sm:text-left">{description}</p>
+      <p className="text-center text-white sm:text-left">{description}</p>
     </div>
   );
 }
