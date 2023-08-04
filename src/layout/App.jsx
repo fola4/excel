4a86370f713ca499e6e0ab5865c1306de45aeaf0
UnsuspectedNoob <mathersmarshall948@gmail.com>
2023-08-04 08:42:56 +0100

    fix react router scroll

--- src/layout/App.jsx
+++ src/layout/App.jsx
@@ -1,9 +1,15 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Navbar />
