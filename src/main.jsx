64015746c4d3e0e2c1082c72d80ad31e29e4c153
UnsuspectedNoob <mathersmarshall948@gmail.com>
2024-04-06 17:02:48 +0100

    Muna is back in this ish

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Muna is back in this ish.
 
 import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider,
-} from 'react-router-dom';
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-import MainPage from './pages/MainPage.jsx';
-import Team from './pages/Team.jsx';
-import Membership from './pages/Membership.jsx';
-import About from './pages/About.jsx';
-import App from './layout/App.jsx';
-import './index.css';
+import MainPage from "./pages/MainPage.jsx";
+import Team from "./pages/Team.jsx";
+import Membership from "./pages/Membership.jsx";
+import About from "./pages/About.jsx";
+import App from "./layout/App.jsx";
+import "./index.css";
 
 const breakpoints = {
-	mobile: 320,
-	mobileLandscape: 480,
-	tablet: 768,
-	tabletLandscape: 1024,
-	desktop: 1200,
-	desktopLarge: 1500,
-	desktopWide: 1920,
+  mobile: 320,
+  mobileLandscape: 480,
+  tablet: 768,
+  tabletLandscape: 1024,
+  desktop: 1200,
+  desktopLarge: 1500,
+  desktopWide: 1920,
 };
 
 const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path="/" element={<App />}>
-			<Route index element={<MainPage />} />
-			<Route path="gallery" element={<Team />} />
-			<Route path="membership" element={<Membership />} />
-			<Route path="about us" element={<About />} />
-			<Route path="thank-you" element={<About />} />
-		</Route>
-	)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<MainPage />} />
+      <Route path="gallery" element={<Team />} />
+      <Route path="membership" element={<Membership />} />
+      <Route path="about us" element={<About />} />
+      <Route path="thank-you" element={<About />} />
+    </Route>
+  )
 );
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
